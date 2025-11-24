37d7ff69c411cb488606cb7cf15fad1e85058cad
ahmdzikri <ahmadzikri.0505@gmail.com>
2025-11-24 22:05:41 +0700

    update closing and header section

--- src/modules/ClosingSection.tsx
+++ src/modules/ClosingSection.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Heart } from "lucide-react";
 import Image from "next/image";
+import Smoke from "~/components/ui/shadcn-io/smoke";
 import config from "~/config/config";
 
 export default function ClosingSection() {
@@ -11,44 +12,40 @@ export default function ClosingSection() {
         bg-primary h-dvh"
     >
       <div className="fixed bottom-0 h-dvh w-full flex flex-col items-center justify-center">
-        <div className="relative z-10 p-4">
-          {/* <div className="mb-6">
-            <div className="w-16 h-16 mx-auto mb-4 relative drop-shadow-lg">
-              <Image
-                src="/placeholder.svg?height=64&width=64"
-                alt="Ornament"
-                width={64}
-                height={64}
-                className="object-contain"
-              />
-            </div>
-          </div> */}
-
-          <div className="mb-8 max-w-md mx-auto bg-white/30 backdrop-blur-sm rounded-2xl p-8 shadow-xl border-8 border-white/50">
-            <h2 className="text-2xl font-serif text-muted mb-4 drop-shadow">
+        <div className="absolute inset-0 pointer-events-none z-0">
+          <Smoke
+            opacity={0.2}
+            density={20}
+            enableRotation={false}
+            enableWind={false}
+            enableTurbulence={false}
+            color="#ffffff"
+          />
+        </div>
+        <div className="relative flex max-w-lg mx-auto aspect-[600/1039] bg-frame-flower-with-rg bg-no-repeat bg-center bg-contain p-4">
+          <div className="flex flex-col gap-4 items-center justify-start px-10 pt-16 xs:pt-24 md:py-30 text-center">
+            <h2 className="text-sm md:text-lg text-foreground uppercase">
               Terima Kasih
             </h2>
-            <p className="text-muted mb-6">
-              Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila
-              Bapak/Ibu/Saudara/i berkenan hadir dan memberikan doa restu.
-            </p>
-            <p className="text-xl font-serif text-muted mb-8 font-semibold">
+            <span className="text-xs md:text-base text-foreground">
+              Tiada yang dapat kami ungkapkan selain rasa syukur, dan tiada yang
+              kami harapkan selain kehadiran dan doa rest
+            </span>
+            <p className="text-3xl font-hello-paris text-foreground mb-6 font-bold">
               {data.brideName} & {data.groomName}
             </p>
-
-            <div className="mt-8 pt-8 border-t border-muted/50">
-              <p className="text-muted italic leading-relaxed">
+            <div className="mt-6">
+              <p className="text-xs md:text-sm text-foreground leading-relaxed">
                 "Dan segala sesuatu Kami ciptakan berpasang-pasangan supaya kamu
                 mengingat kebesaran Allah."
               </p>
-              <p className="text-muted text-sm mt-2 font-medium">
+              <p className="text-xs md:text-sm text-foreground mt-2 font-medium">
                 (QS. Adz-Dzariyat: 49)
               </p>
             </div>
           </div>
         </div>
 
-        
         <div className="absolute bottom-0 left-0 right-0 py-4 px-2 text-center bg-muted">
           <p className="text-foreground text-sm">
             Made with <Heart className="inline-block h-4 w-4 text-foreground" />{" "}
--- src/modules/HeaderSection.tsx
+++ src/modules/HeaderSection.tsx
@@ -18,21 +18,52 @@ export default function HeaderSection({ className = "" }: HeaderSectionProps) {
       className="container-box bg-primary [clip-path:polygon(0_0,100%_0,100%_100%,0_100%)] h-dvh"
     >
       <div
-        className={`fixed bottom-0 h-dvh w-full flex flex-col items-center justify-center text-center px-6 ${className}`}
+        className={`fixed bottom-0 h-dvh w-full flex flex-col items-center justify-center text-center ${className}`}
       >
-        <div className="absolute inset-0 bg-[url('/bg-header.webp')] max-w-[800px] mx-auto bg-[length:auto_calc(100dvh-40px)] bg-contain bg-center bg-bottom bg-no-repeat" />
+        <div className="relative flex max-w-lg mx-auto aspect-[600/1039] bg-frame-flower bg-no-repeat bg-center bg-contain p-4">
+          <div className="flex flex-col items-center justify-center">
+            <motion.div
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 0.6, duration: 0.8 }}
+              className="flex flex-col gap-2 items-center justify-center px-10 text-center"
+            >
+              <p className="text-primary text-sm tracking-widest uppercase -mb-2">
+                Undangan Pernikahan
+              </p>
+              <h1 className="text-5xl font-hello-paris font-bold text-primary tracking-tighter mb-1">
+                {config.couple.brideName} & {config.couple.groomName}
+              </h1>
+              <p className="text-primary/80 text-sm tracking-widest">
+                {formatHeaderDate()}
+              </p>
+            </motion.div>
 
-        <div className="pointer-events-none absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/3 z-20 min-w-[800px]">
-          <motion.div
-            initial={{ y: 300 }}
-            animate={{ y: 0 }}
-            transition={{ duration: 1, ease: [0, 0.71, 0.2, 1.01] }}
-            className="w-full bg-[url('/flower-bottom.webp')] h-[50dvh] bg-contain  bg-center bg-no-repeat"
-          />
+            <motion.div
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ delay: 0.9, duration: 0.8 }}
+            >
+              <div className="animate-bounce mt-10">
+                <ChevronDown
+                  className="mx-auto text-primary hidden md:block"
+                  size={24}
+                  aria-label="Scroll down indicator"
+                />
+                <ChevronUp
+                  className="mx-auto text-primary md:hidden"
+                  size={24}
+                  aria-label="Swipe up indicator"
+                />
+                <p className="text-xs text-primary mt-2 tracking-widest">
+                  <span className="hidden md:inline">SCROLL</span>
+                  <span className="md:hidden">SWIPE</span>
+                </p>
+              </div>
+            </motion.div>
+          </div>
         </div>
-        <div
-          className="pointer-events-none fixed inset-x-0 bottom-0 z-30 h-[50dvh] [mask-image:linear-gradient(to_top,rgba(0,0,0,1)_70%,rgba(0,0,0,.85)_82%,rgba(0,0,0,.6)_92%,rgba(0,0,0,.35)_98%,transparent_100%)] [-webkit-mask-image:linear-gradient(to_top,rgba(0,0,0,1)_50%,rgba(0,0,0,.85)_62%,rgba(0,0,0,.6)_72%,rgba(0,0,0,.35)_82%,transparent_100%)] [mask-repeat:no-repeat][-webkit-mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-size:100%_100%]"
-        >
+        <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 h-[50dvh] [mask-image:linear-gradient(to_top,rgba(0,0,0,1)_70%,rgba(0,0,0,.85)_82%,rgba(0,0,0,.6)_92%,rgba(0,0,0,.35)_98%,transparent_100%)] [-webkit-mask-image:linear-gradient(to_top,rgba(0,0,0,1)_50%,rgba(0,0,0,.85)_62%,rgba(0,0,0,.6)_72%,rgba(0,0,0,.35)_82%,transparent_100%)] [mask-repeat:no-repeat][-webkit-mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-size:100%_100%]">
           <Smoke
             className="w-full h-full"
             opacity={0.2}
@@ -43,54 +74,6 @@ export default function HeaderSection({ className = "" }: HeaderSectionProps) {
             color="#ffffff"
           />
         </div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-          className="relative z-10 max-w-md"
-        >
-          <motion.div
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ delay: 0.6, duration: 0.8 }}
-            className="mb-8"
-          >
-            <p className="text-primary text-sm tracking-widest uppercase">
-              Undangan Pernikahan
-            </p>
-            <h1 className="text-5xl font-hello-paris font-bold text-primary tracking-tighter mb-2">
-              {config.couple.brideName} & {config.couple.groomName}
-            </h1>
-            <p className="text-primary/80 text-lg tracking-widest">
-              {formatHeaderDate()}
-            </p>
-          </motion.div>
-
-          <motion.div
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ delay: 0.9, duration: 0.8 }}
-            className="mt-8"
-          >
-            <div className="animate-bounce mt-16">
-              {/* Desktop: ChevronDown, Mobile/Tablet: ChevronUp */}
-              <ChevronDown
-                className="mx-auto text-primary hidden md:block"
-                size={24}
-                aria-label="Scroll down indicator"
-              />
-              <ChevronUp
-                className="mx-auto text-primary md:hidden"
-                size={24}
-                aria-label="Swipe up indicator"
-              />
-              <p className="text-xs text-primary mt-2 tracking-widest">
-                <span className="hidden md:inline">SCROLL</span>
-                <span className="md:hidden">SWIPE</span>
-              </p>
-            </div>
-          </motion.div>
-        </motion.div>
       </div>
     </motion.div>
   );
